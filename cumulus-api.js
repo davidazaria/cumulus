2214af36eb694183a2d0c00c68a9ccd9f5a7b625
= <azaria.david@gmail.com>
2017-11-30 14:13:29 -0500

    troubleshooting axios connection

--- cumulus-api.js
+++ cumulus-api.js
@@ -6,25 +6,24 @@ const express = require('express');
 // we'll need either request or axis to make promise-based requests
 const cumulusAPI = express.Router();
 const axios = require('axios');
-const APIKEY = process.env.APIKEY;
 
+require('dotenv').config();
+
+const apikey = process.env.APIKEY;
 // hit the API and get back an array of results
 
-axios.get(`https://newsapi.org/v2/top-headlines?language=en&apiKey=${APIKEY}`)
+axios.get(`https://newsapi.org/v2/top-headlines?language=en&apiKey=${apikey}`)
   /* grab the articles from the response data */
   .then(({ data: { articles } }) => {
     /* slice the articles into a manageable size  */
-    const description = articles.slice(1, 10)
+    const description = articles.slice(1,2)
       /* map over and extract only the description from each article */
       .map(({ description }) => description);
-    // map over the array of Urls, invoke axios.get for each,
-    // wrap the whole thing in Promise.all
-    // we'll get back an array of pending promises
     debugger;
     return Promise.all(description.map(description => axios.get(description)));
-  })
+  });
 
   /* catch any errors from anything above */
-  .catch(err => console.error(err));
+// .catch(err => console.error(err));
 
 module.exports = cumulusAPI;
