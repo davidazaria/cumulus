ac972e3293dd2874604590c14049e5fd5ebca972
= <azaria.david@gmail.com>
2017-12-01 13:31:27 -0500

    can consolelog the tfidfMatrix, but tokenizing is continuing to fail. opened a ticket.

--- services/cumulus-api.js
+++ services/cumulus-api.js
@@ -8,6 +8,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const natural = require('natural');
+const tokenizer = new natural.WordTokenizer();
 
 const apikey = process.env.APIKEY;
 // hit the API and get back an array of results
@@ -19,26 +20,25 @@ function hitAxios(req, res, next) {
       apikey,
     },
   })
-
   /* grab the articles from the response data */
     .then(({ data: { articles } }) => {
     /* slice the articles into a manageable size  */
-      const tfidfMatrix = articles.slice(1, 500)
+      const tfidfMatrix = articles.slice(1, 3)
       /* map over and extract only the description from each article */
         .map(({ description, title }) => title + description);
       //   /* TODO []: tokenize, stem, and then rejoining as a new document */
+
       //   .reduce((t, doc) => {
       //     t.addDocument(doc);
       //     return t;
       //   }, natural.tfidf);
       // // return ()
       res.json(tfidfMatrix);
-      debugger;
+      console.log(tfidfMatrix);
     })
     .catch(err => res.send(err));
   /* catch any errors from anything above */
 }
-
 router.get('/', hitAxios);
 
 module.exports = router;
