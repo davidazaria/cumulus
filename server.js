7bf0072ce5b21284eae32cac7f69135b65011a54
= <azaria.david@gmail.com>
2017-12-01 11:11:52 -0500

    trying again to add new files up to git

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
 /* set up logging */
 app.use(logger('dev'));
 
